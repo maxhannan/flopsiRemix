f559dc10b484ddd08338180067faf977c9300065
maxhannan <maxhannan@substanceseven.com>
2023-03-24 18:48:25 -0500

    splash

--- app/routes/App/recipes/$recipeId.jsx
+++ app/routes/App/recipes/$recipeId.jsx
@@ -47,15 +47,6 @@ export const loader = async ({ request, params }) => {
   return { recipe, recipeList, user };
 };
 
-export function meta() {
-  return {
-    // <meta name="description" content="Welcome to the web!" />
-    "apple-mobile-web-app-capable": "yes",
-    // <meta name="theme-color" content="#f22" />
-    "apple-mobile-web-app-status-bar-style": "default",
-  };
-}
-
 export const ErrorBoundary = () => {
   return (
     <>
